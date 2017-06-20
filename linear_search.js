700396d83e213cc08c815d05ed40189b5e1c698c
sidikriders <sidik.riders@gmail.com>
2017-06-20 11:42:10 +0700

    Linear search selesai

--- linear_search.js
+++ linear_search.js
@@ -3,19 +3,32 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
+  for (var i = 0; i < values.length; i++) {
+    if(values[i] == target) {
+      return i
+
+    }
+  } return -1
   //write your code here
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
 
-console.log(linearSearch(18, random_numbers));
+console.log(linearSearch(19, random_numbers));
 // 2
 console.log(linearSearch(9, random_numbers));
 // -1
 
 // Release 1
 console.log("Global Linear Search")
+var index = [];
 let globalLinearSearch = (target, values) => {
+  for (var i = 0; i < values.length; i++) {
+    if(values[i] == target) {
+      index.push(i);
+    }
+  }
+  return index
   //write your code here
 }
 
