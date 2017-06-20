7aa3548deb5ec9e8add9c47cea39f170b03d9289
aridwia <aridwiananto@gmail.com>
2017-06-20 17:03:40 +0700

    linear-search

--- linear_search.js
+++ linear_search.js
@@ -4,7 +4,25 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+ // let jumlah = 0;
+  for(let i=0;i<random_numbers.length;i++){
+    if(random_numbers[i] == target){
+      return i;
+    }
+  // if(random_numbers[random_numbers.length-1] != target){
+  //   return -1;
+//console.log('isi i adalah ' + i)
+  //  jumlah = i
+  }
+
+  // if(jumlah == random_numbers.length-1){
+  return -1;
+  // }
+
+//console.log ('ini adalah ' + jumlah)
 }
+
+
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
 
@@ -17,6 +35,13 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let indexSama = [];
+  for(let i=0;i<banana_arr.length;i++){
+    if(banana_arr[i] == target){
+      indexSama.push(i)
+    }
+  }
+  return indexSama;
 }
 
 let banana_arr = "banana".split("");
