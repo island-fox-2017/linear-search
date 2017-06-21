f3c5ac77f0fe24289b67973236375ab740c00d93
hariantara <hariantara.iputu@gmail.com>
2017-06-21 10:40:11 +0700

    linear-search selesai

--- linear_search.js
+++ linear_search.js
@@ -1,22 +1,49 @@
 'use strict'
 
 // Release 0
+//# buat looping untuk perulangan dan hitung pajang dari values
+//# buat kondisi untuk cek param target sama atau tidak dengan param values
+//# jika sama, tampilkan index keberapa
+//# jika tidak tampilkan -1
+
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+
+  for(let i=0; i<values.length; i++)
+  {
+    while(target == values[i])
+    {
+      return i;
+    }
+  }
+  return -1;
+
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
 
 console.log(linearSearch(18, random_numbers));
-// 2
+2
 console.log(linearSearch(9, random_numbers));
-// -1
+-1
 
-// Release 1
+//Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+//  write your code here
+//console.log(values);
+let push = [];
+for(let i=0; i<banana_arr.length; i++)
+{
+  if(target == banana_arr[i])
+  {
+     push.push(i)
+
+  }
+}
+return push;
+
 }
 
 let banana_arr = "banana".split("");
@@ -24,7 +51,7 @@ let banana_arr = "banana".split("");
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
 
-module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
+// module.exports = {
+//   linearSearch,
+//   globalLinearSearch
+// }
